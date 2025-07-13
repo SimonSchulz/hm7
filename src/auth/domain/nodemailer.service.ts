d648d2ba1eb85f5a5085e07259557dee67fac040
SimonSchulz <zenkov241091@gmail.com>
2025-07-13 17:36:23 +0200

    fix transporter

--- src/auth/domain/nodemailer.service.ts
+++ src/auth/domain/nodemailer.service.ts
@@ -1,32 +1,38 @@
 import nodemailer from "nodemailer";
 import { SETTINGS } from "../../core/setting/setting";
 
-const getTransporter = () => {
-  return nodemailer.createTransport({
-    host: process.env.SMTP_HOST || 'localhost',
-    port: Number(process.env.SMTP_PORT) || 1025,
-    secure: process.env.SMTP_SECURE === 'true', // false для MailDev
-    auth: process.env.EMAIL_PASS
-      ? {
-        user: process.env.EMAIL,
-        pass: process.env.EMAIL_PASS,
-      }
-      : undefined,
-  });
-};
-
+let transporter = nodemailer.createTransport({
+  host: "smtp.yandex.ru",
+  port: 465,
+  secure: true,
+  auth: {
+    user: SETTINGS.EMAIL,
+    pass: SETTINGS.EMAIL_PASS,
+  },
+});
 export const nodemailerService = {
   async sendEmail(
     email: string,
     code: string,
-    template: (code: string) => string
+    template: (code: string) => string,
   ): Promise<void> {
-    const transporter = getTransporter();
-
     await transporter.sendMail({
-      from: `"Blogs platform" <${SETTINGS.EMAIL}>`,
+      from: `"Blogs platform" <${SETTINGS.EMAIL}>`, // обратные кавычки и <...>
       to: email,
-      subject: 'Your account confirmation code',
+      subject: "Your account confirmation code",
+      html: template(code),
+    });
+  },
+
+  async resendEmail(
+    email: string,
+    code: string,
+    template: (code: string) => string,
+  ): Promise<void> {
+    await transporter.sendMail({
+      from: `"Blogs platform hw7" <${SETTINGS.EMAIL}>`, // тоже исправлено
+      to: email,
+      subject: "Email confirmation",
       html: template(code),
     });
   },
