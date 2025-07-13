0d894a47f401b99a781aeadadcfc79c678bd5853
SimonSchulz <zenkov241091@gmail.com>
2025-07-13 09:23:59 +0200

    fix token exp date

--- src/core/setting/setting.ts
+++ src/core/setting/setting.ts
@@ -5,7 +5,7 @@ export const SETTINGS = {
     "mongodb+srv://zenkov241091:IXfaVo0Y7hJuifvB@cluster0.rkqmdcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   DB_NAME: process.env.DB_NAME || "blogger_platform",
   AC_SECRET: process.env.AC_SECRET ||'f1f5deg4hy5fr5d5g',
-  AC_TIME: process.env.AC_TIME || 300,
+  AC_TIME: process.env.AC_TIME || 600,
   EMAIL: process.env.EMAIL as string || 'testovichtest107@gmail.com',
   EMAIL_PASS: process.env.EMAIL_PASS as string || 'typj whjv vrcg aksj',
 };
